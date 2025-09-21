10bb0676d4a3f920ab6bdcbcfdd18cf3ee85882f
amisalem91993 <amisalem91993@gmail.com>
2025-09-21 18:08:09 +0200

    opmerking over taal in settings.py toegevoegd

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # homepage shows blog posts
+    path('', include('blog.urls')),  # homepage goes to blog
 ]
